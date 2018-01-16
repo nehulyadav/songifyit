9e1929b38ed19a03ebd62af59ce8938b5a2c1cf3
Nehul Yadav <nehul12@gmail.com>
2018-01-16 22:10:25 +0530

    in

---
+++ code.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-firestore.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js"></script>
+<script>
+  // Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBaHSrorPiRAu3pXRD-4FMwmCxJAizRbss",
+    authDomain: "presentify-ee169.firebaseapp.com",
+    databaseURL: "https://presentify-ee169.firebaseio.com",
+    projectId: "presentify-ee169",
+    storageBucket: "",
+    messagingSenderId: "1045114376426"
+  };
+  firebase.initializeApp(config);
+
+function writeUserData(name) {
+  var database = firebase.database();
+  firebase.database().ref('users/' + name).set({
+    title: " "
+  });
+}
+
+</script>
+
+
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<script type="text/javascript">
+	function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+</script>
+
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Songify It</a>
+    </div>
+   
+    <form class="navbar-form navbar-left" action="/action_page.php">
+      <div class="form-group">
+        <input type="text" id="inp2" class="form-control" placeholder="Search">
+      </div>
+      <button id="submit" type="submit" class="btn btn-default">Submit</button>
+    </form>
+  </div>
+</nav>
+
+<div id="d" class="container" style="margin-top:100px">
+</div>
+
+ </body>
+
+
+<?php
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://www.googleapis.com/youtube/v3/search?part=snippet&q=wolve&key=AIzaSyCr46o3s9BFdDeDPLCVMmr7lsQphFx2KzI");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+
+//echo $result;
+
+echo '
+<script>
+$.each(' . $result . ', function(i, field){
+					if (i == "items") {
+               			// console.log(field);
+               			$.each(field, function(j, field2){
+                //console.log(j + " " + field2);
+                $.each(field2, function(k, field3){
+                		if (k == "id") {
+							setCookie("ide", field3["videoId"], 365);
+                		}
+
+                		if (k == "snippet") {
+                			var url = "\"" + field3["thumbnails"]["high"]["url"] + "\"";
+                			console.log(field3["thumbnails"]["high"]["url"]);
+
+                			var height = "\"" + "137.64" + "\"";
+                			var width = "\"" + "246" + "\"";
+                			var title = field3["title"];
+                			// var y = "https://www.youtube.com/watch?v=" +  
+                			var c = field3["channelTitle"];
+                			console.log(field3["title"]);
+							console.log(field3["channelTitle"]);
+							                		
+							var str = "<img src=" + url + " height =" + height + " width = " + width + "/><b><a href=\"#\" class=\"testClick\" name=\"" + getCookie("ide")+ "\">" + title + "</a></b></br></br>";
+							document.getElementById("d").innerHTML += str;
+							
+
+                		}
+
+                	
+            });
+
+            });
+
+					}
+            });
+
+
+$("#submit").click(function(){
+  if (document.getElementById("inp2").value == "") {
+    alert("don\'t you have a song to search?");
+  } else {
+    
+    
+}
+
+});
+
+
+
+</script> 
+
+
+';
+
+echo '
+<script>
+
+$(".testClick").click(function () {
+        var addressValue = $(this).attr("name");
+        //alert(addressValue );
+        location.replace("https://www.youtube.com/watch?v="+addressValue+"?rel=0&autoplay=1");
+    });
+
+</script>
+
+';
+
+
+?>
