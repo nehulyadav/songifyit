ffb87a51bd350b9d02aa48514f573f52fe285080
Nehul Yadav <nehul12@gmail.com>
2018-01-16 22:59:37 +0530

    in

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
+<script src='//vws.responsivevoice.com/v/e?key=lGte4szv'></script>
+<script src="http://code.responsivevoice.org/responsivevoice.js"></script>
 <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
 <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js"></script>
 <script src="https://www.gstatic.com/firebasejs/4.8.1/firebase-auth.js"></script>
@@ -29,17 +31,9 @@ function writeUserData(name) {
 </script>
 
 
-<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
 
 <script type="text/javascript">
-	function setCookie(cname, cvalue, exdays) {
+  function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
@@ -62,6 +56,27 @@ function getCookie(cname) {
     return "";
 }
 </script>
+
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<script type="text/javascript">
+if (getCookie("person") == "") {
+  var person = prompt("Please enter your name", "");
+  if (person != null) {
+    setCookie("person", person, 365);
+  }
+}
+
+
+</script>
+
 
 
 </head>
@@ -106,39 +121,39 @@ curl_close ($ch);
 echo '
 <script>
 $.each(' . $result . ', function(i, field){
-					if (i == "items") {
-               			// console.log(field);
-               			$.each(field, function(j, field2){
+          if (i == "items") {
+                    // console.log(field);
+                    $.each(field, function(j, field2){
                 //console.log(j + " " + field2);
                 $.each(field2, function(k, field3){
-                		if (k == "id") {
-							setCookie("ide", field3["videoId"], 365);
-                		}
+                    if (k == "id") {
+              setCookie("ide", field3["videoId"], 365);
+                    }
 
-                		if (k == "snippet") {
-                			var url = "\"" + field3["thumbnails"]["high"]["url"] + "\"";
-                			console.log(field3["thumbnails"]["high"]["url"]);
+                    if (k == "snippet") {
+                      var url = "\"" + field3["thumbnails"]["high"]["url"] + "\"";
+                      console.log(field3["thumbnails"]["high"]["url"]);
 
-                			var height = "\"" + "137.64" + "\"";
-                			var width = "\"" + "246" + "\"";
-                			var title = field3["title"];
-                			// var y = "https://www.youtube.com/watch?v=" +  
-                			var c = field3["channelTitle"];
-                			console.log(field3["title"]);
-							console.log(field3["channelTitle"]);
-							                		
-							var str = "<img src=" + url + " height =" + height + " width = " + width + "/><b><a href=\"#\" class=\"testClick\" name=\"" + getCookie("ide")+ "\">" + title + "</a></b></br></br>";
-							document.getElementById("d").innerHTML += str;
-							
+                      var height = "\"" + "137.64" + "\"";
+                      var width = "\"" + "246" + "\"";
+                      var title = field3["title"];
+                      // var y = "https://www.youtube.com/watch?v=" +  
+                      var c = field3["channelTitle"];
+                      console.log(field3["title"]);
+              console.log(field3["channelTitle"]);
+                                  
+              var str = "<img src=" + url + " height =" + height + " width = " + width + "/><b><a href=\"#\" class=\"testClick\" name=\"" + getCookie("ide")+ "\">" + title + "</a></b></br></br>";
+              document.getElementById("d").innerHTML += str;
+              
 
-                		}
+                    }
 
-                	
+                  
             });
 
             });
 
-					}
+          }
             });
 
 
@@ -164,7 +179,8 @@ echo '
 
 $(".testClick").click(function () {
         var addressValue = $(this).attr("name");
-        //alert(addressValue );
+        alert(addressValue);
+        responsiveVoice.speak(getCookie("person") + "added a song to play now");
         location.replace("https://www.youtube.com/watch?v="+addressValue);
     });
 
